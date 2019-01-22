f7394523991968e6e428ba733e6bd0b49b7743f9
Andranique Boone <andranique.boone@gmail.com>
2019-01-22 02:41:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(respond)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if respond.upcase
+if respond.upcase != 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
