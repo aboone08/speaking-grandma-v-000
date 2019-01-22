6f3e6087f6af1190a68fe217c5b682d1095f22ac
Andranique Boone <andranique.boone@gmail.com>
2019-01-22 02:44:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,5 +12,6 @@ def speak_to_grandma(respond)
 return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
   elsif respond == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 # 'I LOVE YOU TOO PUMPKIN!'
 end 
